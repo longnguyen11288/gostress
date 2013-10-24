7470618f978480c0713141ccf02837abb945d56a
Sahid Orentino Ferdjaoui <sahid.ferdjaoui@cloudwatt.com>
2013-10-24 16:27:50 +0200

    Review options

--- client.go
+++ client.go
@@ -64,7 +64,7 @@ func ready_to_work(id int, conn *Connection) {
 	if debug { log.Printf("Client %d ready to work...", id) }
 	
 	p.subscribe <- conn
-	zap(id, conn)
+	//zap(id, conn)
 	p.unsubscribe <- conn
 	
 	conn.ws.Close()
